ef708a8e84c6cc60d3caca86d21afcf20052641b
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-16 15:54:21 +0700

    prosedur tidak tersusun

--- buble_sort/buble_sort.cpp
+++ buble_sort/buble_sort.cpp
@@ -37,5 +37,14 @@ void bubbleSortArray() {
 	}
 }
 
-
+void unsorted() {
+	cout << endl;
+	cout << "================================" << endl;
+	cout << "Elemen array yang belum tersusun" << endl;
+	cout << "================================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << a[j] << endl;
+	}
+	cout << endl;
+}
 
