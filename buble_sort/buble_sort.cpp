1cca08705ea5289c9f40093f776a12a2ea9df53d
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-16 15:39:02 +0700

    memasukkan bilangan

--- buble_sort/buble_sort.cpp
+++ buble_sort/buble_sort.cpp
@@ -20,4 +20,9 @@ void input() {
 	cout << " Masukkan Elemen Array" << endl;
 	cout << "====================" << endl;
 
+	for (int i = 1; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
 }
+
