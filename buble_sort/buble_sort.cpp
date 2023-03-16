32572fd2246893b5a821cf5c44c5d56acd6ec4d8
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-16 18:34:24 +0700

    Mengubah 1 jadi 0

--- buble_sort/buble_sort.cpp
+++ buble_sort/buble_sort.cpp
@@ -20,7 +20,7 @@ void input() {
 	cout << " Masukkan Elemen Array" << endl;
 	cout << "====================" << endl;
 
-	for (int i = 1; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		cout << "Data ke-" << (i + 1) << ": ";
 		cin >> a[i];
 	}
